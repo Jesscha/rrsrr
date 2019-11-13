90790a9abe75216c25c1ea1d3c93de1070173bc7
jessecha <jessecha222@gmail.com>
2019-11-13 19:41:13 +0900

    에러 복구를 위한 되돌림

--- rrtrr/rrtrr/content/models.py
+++ rrtrr/rrtrr/content/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# from core.models import TimeStampModel
+
+class Contents(models.Model):
+    title = models.CharField(max_length=200, verbose_name = '제목')
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='등록 날짜')
+    # like_count = models.PositiveIntegerField(default=0)
