fa30aad62151b98ce93c7869264e73349011bea0
jessecha <jessecha222@gmail.com>
2019-11-18 14:06:14 +0900

    jquey를 이용한 날짜 달력 구현

--- rrtrr/rrtrr/contents/forms.py
+++ rrtrr/rrtrr/contents/forms.py
@@ -4,18 +4,13 @@ from .models import Post
 class PostModelForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = [
-            'title',
-            'pub_date',
-            'text',
-            'image'
-        ]
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.fields['title'].widget.attrs.update({'class': 'special'})
-            self.fields['pup_date'].widget.attrs.update(size='40')
-
+        fields = '__all__'
+        widgets = {
+            'pub_date': forms.TextInput(attrs=
+                                        {
+                                            'class':'datepicker'
+                                        }),
+        }
 class RawPostModelForm(forms.Form):
     title = forms.CharField()
     pup_date = forms.DateTimeField()
